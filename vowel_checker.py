48c83d070f3ba67fbacc664a3a40d2f8a4708edb
ankitmanjrekar1 <ankitmanjrekar34@gmail.com>
2026-03-28 20:43:41 +0530

    Add vowel checker program

---
+++ vowel_checker.py
@@ -0,0 +1,6 @@
+student_query = input("Enter your letter here: ")
+vowels = ["a", "e', "i", "o", "u", "A", "E", "I", "O", "U"]
+if student_query in vowels:
+    print("vowel")
+else: 
+    print("Not a vowel")
